61707cb41f0773cdce6ffd711abd52cdd530c34a
Riccardo Boselli <r.boselli@codermine.com>
2018-10-30 21:51:25 +0100

    :sparkles: social sharing

--- app/src/main/java/com/quintaooh/thegamereminder/view/activity/GRFragmentActivity.java
+++ app/src/main/java/com/quintaooh/thegamereminder/view/activity/GRFragmentActivity.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import com.quintaooh.thegamereminder.R;
 
 /**
  * Created by riccardo on 30/10/2018.
---
+++ app/src/main/java/com/quintaooh/thegamereminder/view/fragment/GRFragment.java
@@ -0,0 +1,10 @@
+package com.quintaooh.thegamereminder.view.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by riccardo on 30/10/2018.
+ */
+
+public class GRFragment extends Fragment {
+}
